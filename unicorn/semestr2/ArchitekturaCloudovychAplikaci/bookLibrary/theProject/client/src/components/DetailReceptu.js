70dea976cdccc2fa6ac196a11ae449cb6f1b6618
The Avarga <theavarga@the-macbook-air.home>
2021-04-17 21:26:46 +0200

    amaximum of changes

--- unicorn/semestr2/ArchitekturaCloudovychAplikaci/bookLibrary/theProject/client/src/components/DetailReceptu.js
+++ unicorn/semestr2/ArchitekturaCloudovychAplikaci/bookLibrary/theProject/client/src/components/DetailReceptu.js
@@ -5,7 +5,7 @@ const DetailReceptu = () => {
     return (
         <div>
             <h1>{vyhledaneRecepty.data?vyhledaneRecepty.data[zvolenyRecept].nazevReceptu:<></>}</h1>
-            <div>{vyhledaneRecepty.data?<img width="200" src={vyhledaneRecepty.data[zvolenyRecept].nahledovyObrazek} />:"Ahoj"}</div>
+            <div>{vyhledaneRecepty.data?<img width="200" src={vyhledaneRecepty.data[zvolenyRecept].nahledovyObrazek} alt={vyhledaneRecepty.data[zvolenyRecept].nahledovyObrazek}/>:"Ahoj"}</div>
             <p dangerouslySetInnerHTML={{__html:vyhledaneRecepty.data?vyhledaneRecepty.data[zvolenyRecept].popis:<></>}}></p>
         </div>
     )
